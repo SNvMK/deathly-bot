22e5c6ee3e3fbaca93fc6160902189841b7f23f4
SNVMK <sidzahroman@gmail.com>
2021-02-12 15:49:46 +0700

    Update main.py

--- deathly-bot/main.py
+++ deathly-bot/main.py
@@ -34,6 +34,8 @@ async def пинг(ctx):
     Понг!
     """
 
+    await ctx.ack()
+
     embed = discord.Embed(
         title=f"Понг! Пинг сокета: {round(bot.latency) * 1000} мс",
         color=discord.Color.blurple()
