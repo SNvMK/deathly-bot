620190a2f13fe067c63e058b2824531e5be4997f
SNVMK <sidzahroman@gmail.com>
2021-02-14 17:40:03 +0700

    a

--- main.py
+++ main.py
@@ -52,6 +52,7 @@ async def on_ready():
         async with pool.acquire() as conn:
             async with conn.cursor() as cur:
                 injection = """
+                DROP TABLE tags;
                 CREATE TABLE tags (
                     author bigint,
                     name text,
